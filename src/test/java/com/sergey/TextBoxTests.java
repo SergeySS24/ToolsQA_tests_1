b16057b99e25e506e88b97bbd557ae03982519d5
Sergey Starostin <adelaide.star@gmail.com>
2022-05-25 23:15:50 +0300

    added test for form

--- src/test/java/com/sergey/TextBoxTests.java
+++ src/test/java/com/sergey/TextBoxTests.java
@@ -1,12 +1,28 @@
 package com.sergey;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/text-box");
+        open("/text-box");
+
+        $("[id=userName}").setValue("Alex Jones");
+        $("[id=userEmail}").setValue("alex@jones");
+        $("[id=currentAddress}").setValue("Some street 1");
+        $("[id=permanentAddress}").setValue("Another street 2");
+        $("[id=submit}").click();
     }
 }
