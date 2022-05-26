c2694fb29e90a44f7a74a12914651e95bffb8fea
Sergey Starostin <adelaide.star@gmail.com>
2022-05-26 23:16:42 +0300

    added another tests

--- src/test/java/com/sergey/TextBoxTests.java
+++ src/test/java/com/sergey/TextBoxTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,16 +14,23 @@ public class TextBoxTests {
     static void setUp() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
     void fillFormTest() {
+        String name = "Alex Jones"; //использует name в качестве alias и подтягивает соответственное значение в код
+
         open("/text-box");
 
-        $("[id=userName}").setValue("Alex Jones");
-        $("[id=userEmail}").setValue("alex@jones");
-        $("[id=currentAddress}").setValue("Some street 1");
-        $("[id=permanentAddress}").setValue("Another street 2");
-        $("[id=submit}").click();
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue("alex@jones.com");
+        $("[id=currentAddress]").setValue("Some street 1");
+        $("[id=permanentAddress]").setValue("Another street 2");
+        $("[id=submit]").click();
+
+        $("[id=output]").shouldHave(text(name), text("alex@jones.com"),
+                text("Some street 1"), text("Another street 2"));
+
     }
 }
