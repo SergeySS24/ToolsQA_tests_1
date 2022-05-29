cb6eb21435b5948aa996b771c61b6bffaa9dd9c9
Sergey Starostin <adelaide.star@gmail.com>
2022-05-29 21:48:40 +0300

    added lots of asserts

--- src/test/java/com/sergey/TextBoxTests.java
+++ src/test/java/com/sergey/TextBoxTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TextBoxTests {
 
@@ -29,8 +30,23 @@ public class TextBoxTests {
         $("[id=permanentAddress]").setValue("Another street 2");
         $("[id=submit]").click();
 
+        //Asserts
         $("[id=output]").shouldHave(text(name), text("alex@jones.com"),
-                text("Some street 1"), text("Another street 2"));
+                         text("Some street 1"), text("Another street 2"));
+
+  //      $("[id=output] [id=name]").shouldHave(text(name)); //вариант №1 (в поле вывода имя соответствует
+  //      // полю имени)
+  //      $("[id=output]").$("[id=name]").shouldHave(text(name)); // вариант 2
+  //      $("[id=name]").shouldHave(text(name));
+
+  //      $("[id=permanentAddress]").shouldHave(text("permanentAddress :Another street 2")); //wrong
+  //      $("p[id=permanentAddress]").shouldHave(text("permanentAddress :Another street 2")); //p - название элемента
+  //      $("[id=permanentAddress]", 1).shouldHave(text("permanentAddress :Another street 2")); //нужно стараться по индексам не обращаться
+
+        //Старый подход. 3 нижние строчки идентичны строчке 43
+  //      String expectedPermanentAddress = "Another street 2";
+  //      String actualPermanentAddress = $("p[id=permanentAddress]").text();
+  //      assertEquals(expectedPermanentAddress, actualPermanentAddress);
 
     }
 }
