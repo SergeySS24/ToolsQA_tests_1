186338ca8e8b7e1290b098f5a16aef9d506ae23d
Sergey Starostin <adelaide.star@gmail.com>
2022-05-31 19:14:38 +0300

    add final commit

--- src/test/java/com/sergey/docs/CssXpathExamples.java
+++ src/test/java/com/sergey/docs/CssXpathExamples.java
@@ -11,13 +11,40 @@ public class CssXpathExamples {
         $(by("data-testid", "email")).setValue("123"); // способ 2
 
 
-        //<input type="email" class="inputtext login_form_input_box" name="email" id="email">
+        //<input type="email" class="inputtext login_form_input_box" name="email" id="email"> //если нет атрибута testid
         $("[id=email]").setValue("123");
         $("#email").setValue("123"); //на курсе используют этот способ
         $(byId("#email")).setValue("123");
         $x("//*[@id='email']").setValue("123");
         $(byXpath("//*[@id='email']")).setValue("123");
 
+        //<input type="email" class="inputtext login_form_input_box" name="email"> // если нет атрибута id
+        $("[name=email]").setValue("123"); //вариант 1
+        $(byName("email")).setValue("123"); //вариант 2
 
+        //<input type="email" class="inputtext login_form_input_box"> // если нет атрибута name
+        $("[class=login_form_input_box]").setValue("123"); // вариант 1
+        $(".inputtext.login_form_input_box").setValue("123"); // вариант 2
+        $("input.inputtext.login_form_input_box").setValue("123"); // вариант 3
+        $(byXpath("//input[@class='login_form_input_box']")).setValue("123");
+        $(byXpath("//input[@class='inputtext'][@class='login_form_input_box']")).setValue("123");
+
+         //<div> class="inputtext"
+        //      <input type="email" class="login_form_input_box" name="email" id="email">
+        //</div>
+        $(".inputtext.login_form_input_box").setValue("123");
+
+
+        //<input type="email" class="inputtext login_form_input_box" name="email" id="email" data-testid="email">
+        $(".inputtext.login_form_input_box#email[name=email][data-testid=email]").setValue("123"); // используемый
+        // на курсе способ со всеми вставленными атрибутами
+
+        //  <div>Hello qa.guru</div>  // поиск по тексту
+        $x("//div[text()='Hello qa.guru']");
+        $x("//div[contains(text(), 'ello qa.gur']");
+        $x("//div[text()[contains(., 'ello qa.gur')]]");
+
+        $(byText("Hello qa.guru")); //нормальный вариант
+        $(withText("ello qa.gur")); //нормальный вариант
     }
 }
--- src/test/java/com/sergey/tests/TextBoxTests.java
+++ src/test/java/com/sergey/tests/TextBoxTests.java
@@ -33,20 +33,20 @@ public class TextBoxTests {
         //Asserts
         $("[id=output]").shouldHave(text(name), text("alex@jones.com"),
                          text("Some street 1"), text("Another street 2"));
-
-  //      $("[id=output] [id=name]").shouldHave(text(name)); //вариант №1 (в поле вывода имя соответствует
-  //      // полю имени)
-  //      $("[id=output]").$("[id=name]").shouldHave(text(name)); // вариант 2
-  //      $("[id=name]").shouldHave(text(name));
-
-  //      $("[id=permanentAddress]").shouldHave(text("permanentAddress :Another street 2")); //wrong
-  //      $("p[id=permanentAddress]").shouldHave(text("permanentAddress :Another street 2")); //p - название элемента
-  //      $("[id=permanentAddress]", 1).shouldHave(text("permanentAddress :Another street 2")); //нужно стараться по индексам не обращаться
-
-        //Старый подход. 3 нижние строчки идентичны строчке 43
-  //      String expectedPermanentAddress = "Another street 2";
-  //      String actualPermanentAddress = $("p[id=permanentAddress]").text();
-  //      assertEquals(expectedPermanentAddress, actualPermanentAddress);
+//
+//        $("[id=output] [id=name]").shouldHave(text(name)); //вариант №1 (в поле вывода имя соответствует
+//        // полю имени)
+//        $("[id=output]").$("[id=name]").shouldHave(text(name)); // вариант 2
+//        $("[id=name]").shouldHave(text(name));
+//
+//        $("[id=permanentAddress]").shouldHave(text("permanentAddress :Another street 2")); //wrong
+//        $("p[id=permanentAddress]").shouldHave(text("permanentAddress :Another street 2")); //p - название элемента
+//        $("[id=permanentAddress]", 1).shouldHave(text("permanentAddress :Another street 2")); //нужно стараться по индексам не обращаться
+//
+//        Старый подход. 3 нижние строчки идентичны строчке 43
+//        String expectedPermanentAddress = "Another street 2";
+//        String actualPermanentAddress = $("p[id=permanentAddress]").text();
+//        assertEquals(expectedPermanentAddress, actualPermanentAddress);
 
     }
 }
